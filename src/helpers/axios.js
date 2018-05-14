aa4fff392a7b78f7d05dae3edecfb974d82450a3
Shion093 <shion093@gmail.com>
2018-05-13 22:03:08 -0600

    build

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL : 'http://localhost:7777/v1/',
+  baseURL : process.env.NODE_ENV === 'development'
+    ? 'http://localhost:7777/v1/'
+    : 'https://api.bocaapp.com/v1/',
   timeout : 10000,
 });
 
